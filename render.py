ab91631ea279e9eec101b7335db3f17f6fc89d78
Matt Haggard <haggardii@gmail.com>
2013-04-05 23:22:44 -0600

    Simplifications (deleting features) and formatting changes

--- render.py
+++ render.py
@@ -1,9 +1,21 @@
 from jinja2 import Environment, FileSystemLoader
 
 
+def base64enc(s):
+	return s.encode('base64')
+
+
+def base64dec(s):
+	return s.decode('base64')
+
+
 def render(filename):
 	loader = FileSystemLoader('_templates')
 	jenv = Environment(loader=loader)
+	jenv.filters.update({
+		'b64enc': base64enc,
+		'b64dec': base64dec,
+	})
 	template = jenv.get_template(filename)
 	return template.render()
 
